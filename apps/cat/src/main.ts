f20c99976d6f18daadb9e0511cd736bf43680b26
小丑在唱独角戏 <10097251+xczcdjx@user.noreply.gitee.com>
2024-04-25 15:38:53 +0800

    cat 接入swagger ui操作

--- apps/cat/src/main.ts
+++ apps/cat/src/main.ts
@@ -1,12 +1,19 @@
-import { NestFactory } from '@nestjs/core';
-import { CatModule } from './cat.module';
+import { NestFactory } from "@nestjs/core";
+import { CatModule } from "./cat.module";
 import { ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(CatModule);
   app.useGlobalPipes(new ValidationPipe({
-    whitelist:true // 设置白名单不存在dto的属性将不会添加创建依赖
-  }))
+    whitelist: true // 设置白名单不存在dto的属性将不会添加创建依赖
+  }));
+  const document = SwaggerModule.createDocument(app,
+    new DocumentBuilder().addBearerAuth()
+      .setTitle(`cat document`).setDescription(`cat document`)
+      .setVersion("1").build());
+  SwaggerModule.setup("doc", app, document);
   await app.listen(3001);
 }
+
 bootstrap();
--- apps/cat/src/stand/stand.controller.ts
+++ apps/cat/src/stand/stand.controller.ts
@@ -2,17 +2,20 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from "@nestjs/commo
 import { StandService } from "./stand.service";
 import { CreateStandDto } from "./dto/create-stand.dto";
 import { UpdateStandDto } from "./dto/update-stand.dto";
-
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
+@ApiTags('stand')
 @Controller("stand")
 export class StandController {
   constructor(private readonly standService: StandService) {
   }
 
+  @ApiOperation({summary:'增'})
   @Post()
   create(@Body() createStandDto: CreateStandDto) {
     return this.standService.create(createStandDto);
   }
 
+  @ApiOperation({summary:'查'})
   @Get()
   findAll() {
     return this.standService.findAll();
@@ -23,11 +26,13 @@ export class StandController {
     return this.standService.findOne(+id);
   }
 
+  @ApiOperation({summary:'改'})
   @Patch()
   update(@Body() updateStandDto: UpdateStandDto) {
     return this.standService.update(updateStandDto);
   }
 
+  @ApiOperation({summary:'删'})
   @Delete(":id")
   remove(@Param("id") id: string) {
     return this.standService.remove(+id);
